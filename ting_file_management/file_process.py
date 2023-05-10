9b2d79d505a8f087c3b3abe355473c14a5f6d8d5
Edson Pereira <ed.1206@gmail.com>
2023-05-09 22:49:25 -0300

    feat: :sparkles: Implemented `process` function in `file_process` module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    data = txt_importer(path_file)
+    exit_structure = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+
+    instance.enqueue(exit_structure)
+    sys.stdout.write(str(exit_structure))
+    return exit_structure
 
 
 def remove(instance):
